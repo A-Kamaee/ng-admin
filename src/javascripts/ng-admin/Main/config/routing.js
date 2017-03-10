2f06790d6ad22d811f16550d894b3bd02bf0b526
Abouzar <abouzarkamaee@gmail.com>
2017-03-10 16:20:00 +0330

    Add ng-admin-login.

--- src/javascripts/ng-admin/Main/config/routing.js
+++ src/javascripts/ng-admin/Main/config/routing.js
@@ -3,130 +3,196 @@ import dashboardTemplate from '../view/dashboard.html';
 import errorTemplate from '../view/404.html';
 
 function dataStoreProvider() {
-    return ['AdminDescription', function (AdminDescription) {
-        return AdminDescription.getDataStore();
-    }];
+  return [
+    'AdminDescription',
+    function(AdminDescription) {
+      return AdminDescription.getDataStore();
+    },
+  ];
 }
 
 function entryConstructorProvider() {
-    return ['AdminDescription', function (AdminDescription) {
-        return AdminDescription.getEntryConstructor();
-    }];
+  return [
+    'AdminDescription',
+    function(AdminDescription) {
+      return AdminDescription.getEntryConstructor();
+    },
+  ];
 }
 
 function routing($stateProvider, $urlRouterProvider) {
+  $stateProvider.state('ng-admin', {
+    abstract: true,
+    views: {
+      'ng-admin': {
+        controller: 'AppController',
+        controllerAs: 'appController',
+        templateProvider: [
+          'NgAdminConfiguration',
+          function(Configuration) {
+            return Configuration().layout() || layoutTemplate;
+          },
+        ],
+      },
+    },
+  });
 
-    $stateProvider.state('ng-admin', {
-        abstract: true,
-        views: {
-            'ng-admin': {
-                controller: 'AppController',
-                controllerAs: 'appController',
-                templateProvider: ['NgAdminConfiguration', function(Configuration) {
-                    return Configuration().layout() || layoutTemplate;
-                }]
-            }
-        }
-    });
+  $stateProvider.state('ng-admin-login', {
+    abstract: true,
+    views: {
+      'ng-admin-login': {
+        controller: 'AppController',
+        controllerAs: 'appController',
+        templateProvider: [
+          'NgAdminConfiguration',
+          function(Configuration) {
+            return '<div ui-view>';
+          },
+        ],
+      },
+    },
+  });
 
-    $stateProvider.state('dashboard', {
-        parent: 'ng-admin',
-        url: '/dashboard?sortField&sortDir',
-        params: {
-            sortField: null,
-            sortDir: null
+  $stateProvider.state('dashboard', {
+    parent: 'ng-admin',
+    url: '/dashboard?sortField&sortDir',
+    params: {
+      sortField: null,
+      sortDir: null,
+    },
+    controller: 'DashboardController',
+    controllerAs: 'dashboardController',
+    templateProvider: [
+      'NgAdminConfiguration',
+      function(Configuration) {
+        return Configuration().dashboard().template() || dashboardTemplate;
+      },
+    ],
+    resolve: {
+      dataStore: dataStoreProvider(),
+      Entry: entryConstructorProvider(),
+      hasEntities: [
+        'NgAdminConfiguration',
+        function(Configuration) {
+          return Configuration().entities.length > 0;
         },
-        controller: 'DashboardController',
-        controllerAs: 'dashboardController',
-        templateProvider: ['NgAdminConfiguration', function(Configuration) {
-            return Configuration().dashboard().template() || dashboardTemplate;
-        }],
-        resolve: {
-            dataStore: dataStoreProvider(),
-            Entry: entryConstructorProvider(),
-            hasEntities: ['NgAdminConfiguration', function(Configuration) {
-                return Configuration().entities.length > 0;
-            }],
-            collections: ['NgAdminConfiguration', function(Configuration) {
-                return Configuration().dashboard().collections();
-            }],
-            responses: ['$stateParams', '$q', 'collections', 'dataStore', 'Entry', 'ReadQueries', function($stateParams, $q, collections, dataStore, Entry, ReadQueries) {
-                var sortField = 'sortField' in $stateParams ? $stateParams.sortField : null;
-                var sortDir = 'sortDir' in $stateParams ? $stateParams.sortDir : null;
+      ],
+      collections: [
+        'NgAdminConfiguration',
+        function(Configuration) {
+          return Configuration().dashboard().collections();
+        },
+      ],
+      responses: [
+        '$stateParams',
+        '$q',
+        'collections',
+        'dataStore',
+        'Entry',
+        'ReadQueries',
+        function($stateParams, $q, collections, dataStore, Entry, ReadQueries) {
+          var sortField = 'sortField' in $stateParams
+            ? $stateParams.sortField
+            : null;
+          var sortDir = 'sortDir' in $stateParams ? $stateParams.sortDir : null;
 
-                var promises = {},
-                    collection,
-                    collectionSortField,
-                    collectionSortDir,
-                    collectionName;
+          var promises = {},
+            collection,
+            collectionSortField,
+            collectionSortDir,
+            collectionName;
 
-                for (collectionName in collections) {
-                    collection = collections[collectionName];
-                    collectionSortField = collection.getSortFieldName();
-                    collectionSortDir = collection.sortDir();
-                    if (sortField && sortField.split('.')[0] === collection.name()) {
-                        collectionSortField = sortField;
-                        collectionSortDir = sortDir;
-                    }
-                    promises[collectionName] = (function (collection, collectionSortField, collectionSortDir) {
-                        var rawEntries;
+          for (collectionName in collections) {
+            collection = collections[collectionName];
+            collectionSortField = collection.getSortFieldName();
+            collectionSortDir = collection.sortDir();
+            if (sortField && sortField.split('.')[0] === collection.name()) {
+              collectionSortField = sortField;
+              collectionSortDir = sortDir;
+            }
+            promises[collectionName] = (function(
+              collection,
+              collectionSortField,
+              collectionSortDir
+            ) {
+              var rawEntries;
 
-                        return ReadQueries
-                            .getAll(collection, 1, {}, collectionSortField, collectionSortDir)
-                            .then(response => {
-                                rawEntries = response.data;
-                                return rawEntries;
-                            })
-                            .then(rawEntries => ReadQueries.getReferenceData(collection.fields(), rawEntries))
-                            .then(referenceData => {
-                                const references = collection.getReferences();
-                                for (var name in referenceData) {
-                                    Entry.createArrayFromRest(
-                                        referenceData[name],
-                                        [references[name].targetField()],
-                                        references[name].targetEntity().name(),
-                                        references[name].targetEntity().identifier().name()
-                                    ).map(entry => dataStore.addEntry(references[name].targetEntity().uniqueId + '_values', entry));
-                                }
-                            })
-                            .then(() => {
-                                var entries = collection.mapEntries(rawEntries);
+              return ReadQueries.getAll(
+                collection,
+                1,
+                {},
+                collectionSortField,
+                collectionSortDir
+              )
+                .then(response => {
+                  rawEntries = response.data;
+                  return rawEntries;
+                })
+                .then(rawEntries =>
+                  ReadQueries.getReferenceData(collection.fields(), rawEntries))
+                .then(referenceData => {
+                  const references = collection.getReferences();
+                  for (var name in referenceData) {
+                    Entry.createArrayFromRest(
+                      referenceData[name],
+                      [references[name].targetField()],
+                      references[name].targetEntity().name(),
+                      references[name].targetEntity().identifier().name()
+                    ).map(entry =>
+                      dataStore.addEntry(
+                        references[name].targetEntity().uniqueId + '_values',
+                        entry
+                      ));
+                  }
+                })
+                .then(() => {
+                  var entries = collection.mapEntries(rawEntries);
 
-                                // shortcut to display collection of entry with included referenced values
-                                dataStore.fillReferencesValuesFromCollection(entries, collection.getReferences(), true);
+                  // shortcut to display collection of entry with included referenced values
+                  dataStore.fillReferencesValuesFromCollection(
+                    entries,
+                    collection.getReferences(),
+                    true
+                  );
 
-                                return entries;
-                            });
-                    })(collection, collectionSortField, collectionSortDir);
-                }
+                  return entries;
+                });
+            })(collection, collectionSortField, collectionSortDir);
+          }
 
-                return $q.all(promises);
-            }],
-            entries: ['responses', 'collections', function(responses, collections) {
-                var collectionName,
-                    entries = {};
+          return $q.all(promises);
+        },
+      ],
+      entries: [
+        'responses',
+        'collections',
+        function(responses, collections) {
+          var collectionName, entries = {};
 
-                for (collectionName in responses) {
-                    entries[collections[collectionName].name()] = responses[collectionName];
-                }
+          for (collectionName in responses) {
+            entries[collections[collectionName].name()] = responses[
+              collectionName
+            ];
+          }
 
-                return entries;
-            }]
-        }
-    });
+          return entries;
+        },
+      ],
+    },
+  });
 
-    $stateProvider.state('ma-404', {
-        parent: 'ng-admin',
-        template: errorTemplate
-    });
+  $stateProvider.state('ma-404', {
+    parent: 'ng-admin',
+    template: errorTemplate,
+  });
 
-    $urlRouterProvider.when('', '/dashboard');
+  $urlRouterProvider.when('', '/dashboard');
 
-    $urlRouterProvider.otherwise(function($injector, $location) {
-        var state = $injector.get('$state');
-        state.go('ma-404');
-        return $location.path();
-    });
+  $urlRouterProvider.otherwise(function($injector, $location) {
+    var state = $injector.get('$state');
+    state.go('ma-404');
+    return $location.path();
+  });
 }
 
 routing.$inject = ['$stateProvider', '$urlRouterProvider'];
